79de917742a2098fe886cb9b4c59b8f849d5e5ba
Nelson Malbone <nhmalbone@icloud.com>
2024-06-12 16:17:08 -0400

    Personal Website Date 06/12/2024
    added a photo for "who i am"
    added content to this section also

--- about.py
+++ about.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.header('About Me')
+# centering the header
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write('')
+with col2:
+    st.header('About Me')
+    st.subheader('Who I Am?')
+with col3:
+    st.write('')
+
+# who i am section
+col_1, col_2 = st.columns(2)
+with col_1:
+    st.image('images/photo.png', width=275)
+
+with col_2:
+    content = """
+        Hi, my name is Nelson. Reliable Warehouse Worker with extensive experience
+    """
+    content1 = """
+    A person that is eager to learn new things a team player with an eye for detail and safety
+    """
+    content2 = """
+     Self learning how to code using python and along with photography 
+    """
+    st.info(content)
+    st.info(content1)
+    st.info(content2)
+
+# top 3 code section
+
+
+# top 3 photos taken
